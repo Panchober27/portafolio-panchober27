76cf52a9f98dcf3a31e59d9c56dd6bc653b2aecf
Panchober27 <58098908+Panchober27@users.noreply.github.com>
2021-03-17 02:05:00 -0300

    Subida a GitHub🖤

---
+++ src/app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const path = require("path");
+const morgan = require("morgan");
+
+const app = express();
+
+//S
+const port = process.env.PORT || 3000;
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+// Middlewares settings
+app.use(morgan("dev"));
+// future middlewares
+
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+// Routes
+app.use(require('./routes/index'));
+
+
+// LS.
+app.listen(port, () => {
+    console.log(`Server started on port: ${port}`);
+});
