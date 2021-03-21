bece85eede3203940483a179c3fb40d55ba1d888
Panchober27 <58098908+Panchober27@users.noreply.github.com>
2021-03-20 23:28:09 -0300

    Actualización vistas 😎

---
+++ src/public/js/live-demos.js
@@ -0,0 +1,9 @@
+// boton pa avisar que no se ha a añadido contenido alli aun.
+const btnNoData = document.getElementById('btnNoData');
+
+btnNoData.addEventListener('click', () => {
+    alert('aún no hago esto...')
+})
+
+
+
